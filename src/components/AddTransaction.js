97c09833cc9f44673dfa6876a3e6f5cff576ad75
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-10-03 10:47:09 +0700

    Connect user add transaction

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const AddTransaction = () => {
+  const [text, setText] = useState('');
+  const [amount, setAmount] = useState(0);
+
   return (
     <>
       <h3>Add New Transaction</h3>
       <form>
         <div className="form-control">
           <label htmlFor="text">Description</label>
-          <input type="text" placeholder="Write description..." />
+          <input
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Write description..."
+          />
         </div>
         <div className="form-control">
           <label htmlFor="amount">Amount</label>
-          <input type="number" placeholder="Enter amount..." />
+          <input
+            type="number"
+            value={amount}
+            onChange={(e) => setAmount(e.target.value)}
+            placeholder="Enter amount..."
+          />
         </div>
         <button className="btn">Add transaction</button>
       </form>
