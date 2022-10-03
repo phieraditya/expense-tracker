969ad9f5cd4347ca7ba8a0e6a82183ce39dd0117
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-10-03 12:59:27 +0700

    Write context and reducer

---
+++ src/context/GlobalState.js
@@ -0,0 +1,26 @@
+import React, { createContext, useReducer } from 'react';
+import AppReducer from './AppReducer';
+
+// Initial State
+const initialState = {
+  transactions: [
+    { id: 1, text: 'Coffee', amount: -39.6 },
+    { id: 2, text: 'Burger', amount: -43.5 },
+    { id: 3, text: 'Income', amount: 199 },
+    { id: 4, text: 'Eggs', amount: -22.9 },
+  ],
+};
+
+// Create context
+export const GlobalContext = createContext(initialState);
+
+// Provider component
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  return (
+    <GlobalContext.Provider value={{ transactions: state.transactions }}>
+      {children}
+    </GlobalContext.Provider>
+  );
+};
