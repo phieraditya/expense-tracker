9d0f0bcf9c646c531f0c11aba0c3e2ac1a5899f0
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-10-03 13:47:35 +0700

    Refactor TransactionList with Transaction component

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,4 +1,6 @@
 import React, { useContext } from 'react';
+import Transaction from './Transaction';
+
 import { GlobalContext } from '../context/GlobalState';
 
 const TransactionList = () => {
@@ -8,12 +10,8 @@ const TransactionList = () => {
     <>
       <h3>History</h3>
       <ul className="list">
-        {/* Transaction component */}
         {transactions.map((transaction) => (
-          <li key={transaction.id} className="minus">
-            {transaction.text} <span>-Rp 400</span>
-            <button className="delete-btn">x</button>
-          </li>
+          <Transaction key={transaction.id} transaction={transaction} />
         ))}
       </ul>
     </>
