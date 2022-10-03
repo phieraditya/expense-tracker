87763d16b5ec597c6784f106b2747351a51756fc
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-10-03 14:14:09 +0700

    Add balance and total income/expense functionality

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,15 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 const IncomeExpenses = () => {
+  const { transactions } = useContext(GlobalContext);
+
+  const amounts = transactions.map((transaction) => transaction.amount);
+  const income = amounts
+    .filter((amount) => amount > 0)
+    .reduce((acc, amount) => (acc += amount), 0)
+    .toFixed(2);
+  const expense = Math.abs(
+    amounts
+      .filter((amount) => amount < 0)
+      .reduce((acc, expense) => (acc += expense), 0)
+  ).toFixed(2);
   return (
     <div className="inc-exp-container">
       <div>
         <h4>Income</h4>
-        <p className="money plus">+Rp 0.00</p>
+        <p className="money plus">+Rp {income}</p>
       </div>
       <div>
         <h4>Expense</h4>
-        <p className="money minus">-Rp 0.00</p>
+        <p className="money minus">-Rp {expense}</p>
       </div>
     </div>
   );
