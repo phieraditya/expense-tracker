311c58e1706a7ec911412e16bfcc1534fffc8d74
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-11-22 15:04:07 +0700

    Connect to DB

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 
 dotenv.config({ path: './config/config.env' });
+
+connectDB();
 
 const transactions = require('./routes/transactions');
 
@@ -16,6 +19,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
   console.log(
-    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.red.bold
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
   )
 );
