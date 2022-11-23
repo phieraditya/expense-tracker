65ad5bfabeee2e297668dd3084ed157a25dd877b
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-11-23 17:20:54 +0700

    Get transactions from backend

--- client/src/context/GlobalState.js
+++ client/src/context/GlobalState.js
@@ -1,5 +1,6 @@
 import React, { createContext, useReducer } from 'react';
 import AppReducer from './AppReducer';
+import axios from 'axios';
 
 // Initial State
 const initialState = {
@@ -14,6 +15,17 @@ export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
 
   // Actions
+  async function getTransactions() {
+    try {
+      const res = await axios.get('/api/v1/transactions');
+
+      dispatch({
+        type: 'GET_TRANSACTIONS',
+        payload: res.data.data,
+      });
+    } catch (error) {}
+  }
+
   function deleteTransaction(id) {
     dispatch({ type: 'DELETE_TRANSACTION', payload: id });
   }
