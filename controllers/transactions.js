a6f8d2cf9246594512ad8b57d030fee2293457d5
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-11-23 10:43:09 +0700

    Write get transactions method

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -3,20 +3,33 @@ const Transaction = require('../models/Transaction');
 // @desc    Get all transactions
 // @route   GET /api/v1/transactions
 // @access  Public
-exports.getTransactions = (req, res, next) => {
-  res.send('GET transactions');
+exports.getTransactions = async (req, res, next) => {
+  try {
+    const transactions = await Transaction.find();
+
+    return res.status(200).json({
+      success: true,
+      count: transactions.length,
+      data: transactions,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      error: 'Server Error',
+    });
+  }
 };
 
 // @desc    Add transaction
 // @route   POST /api/v1/transactions
 // @access  Public
-exports.addTransaction = (req, res, next) => {
+exports.addTransaction = async (req, res, next) => {
   res.send('POST transaction');
 };
 
 // @desc    Delete transaction
 // @route   DELETE /api/v1/transactions/:id
 // @access  Public
-exports.deleteTransaction = (req, res, next) => {
+exports.deleteTransaction = async (req, res, next) => {
   res.send('DELETE transaction');
 };
